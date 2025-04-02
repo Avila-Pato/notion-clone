76c7a652d2fec11aee03415a60311a2e712f39d7
Avila-Pato <p.avilaf1998@gmail.com>
2025-04-01 22:54:58 -0300

    enhance navbar with scroll effect and styling

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,8 +1,16 @@
 "use client"
 
+import { useScrollTop } from "@/hooks/use-scroll-top"
+import { cn } from "@/lib/utils"
+
 const Navbar = () => {
+const scrolled = useScrollTop()
+
   return (
-    <div>
+    <div className={cn(
+      "z-50 bg-background fixed top-0 flex items-center w-full",
+      scrolled ? "border-b shadow-2xl" : ""
+    )}>
       Navbar
     </div>
   )
